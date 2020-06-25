3e5569ba2b187842752f1e08afae2026be26cbeb
Marcos Couto <marcos_otuoc@hotmail.com>
2020-06-25 11:50:22 -0300

    HttpMessageNotReadableException handler

--- src/main/kotlin/com/github/marcoscouto/cadastrosempresa/resource/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/github/marcoscouto/cadastrosempresa/resource/exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.github.marcoscouto.cadastrosempresa.resource.exception
 import com.github.marcoscouto.cadastrosempresa.exception.NotFoundException
 import com.github.marcoscouto.cadastrosempresa.exception.StandardError
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -15,11 +16,20 @@ class ExceptionHandler {
 
     @ExceptionHandler(NotFoundException::class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    fun NotFoundExceptionHandler(e: NotFoundException): StandardError =
+    fun notFoundExceptionHandler(e: NotFoundException): StandardError =
             StandardError("Informação não encontrada", listOf(e.message!!), timestamp)
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    fun IllegalArgumentException(e: IllegalArgumentException): StandardError =
+    fun illegalArgumentExceptionHandler(e: IllegalArgumentException): StandardError =
             StandardError("Argumento inválido", listOf(e.message!!), timestamp)
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    fun httpMessageNotReadableExceptionHandler(e: HttpMessageNotReadableException): StandardError =
+            StandardError("Erro HTTP", listOf(cleanHttpMessage(e.message!!)), timestamp)
+
+    private fun cleanHttpMessage(message: String) =
+            message.replaceAfter(":", "")
+                    .replace(":", "")
 }
