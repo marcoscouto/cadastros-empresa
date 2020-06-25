83499c5aabf6ef855bd854f7489cfa9637b54cd5
Marcos Couto <marcos_otuoc@hotmail.com>
2020-06-25 10:20:04 -0300

    Populate Database

---
+++ src/main/kotlin/com/github/marcoscouto/cadastrosempresa/config/TestConfig.kt
@@ -0,0 +1,16 @@
+package com.github.marcoscouto.cadastrosempresa.config
+
+import com.github.marcoscouto.cadastrosempresa.utils.DatabaseUtils
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("test")
+class TestConfig(val utils: DatabaseUtils) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        utils.deleteData()
+        utils.populateDatabase()
+    }
+}
---
+++ src/main/kotlin/com/github/marcoscouto/cadastrosempresa/utils/DatabaseUtils.kt
@@ -0,0 +1,20 @@
+package com.github.marcoscouto.cadastrosempresa.utils
+
+import com.github.marcoscouto.cadastrosempresa.domain.Funcionario
+import com.github.marcoscouto.cadastrosempresa.repository.FuncionarioRepository
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseUtils(val funcionarioRepository: FuncionarioRepository) {
+
+    fun populateDatabase(){
+        funcionarioRepository.saveAll(listOf(
+                Funcionario(null, "John",40, "john@empresa.com.br", "Presidente"),
+                Funcionario(null, "Ringo",35, "ringo@empresa.com.br", "Diretor de Tecnologia"),
+                Funcionario(null, "George",32, "george@empresa.com.br", "Diretor de Finanças"),
+                Funcionario(null, "Paul",37, "paul@empresa.com.br", "Diretor Administrativo")))
+    }
+
+    fun deleteData() = funcionarioRepository.deleteAll()
+
+}
