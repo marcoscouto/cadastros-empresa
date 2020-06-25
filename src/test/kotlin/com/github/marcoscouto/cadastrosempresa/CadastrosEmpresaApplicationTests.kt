79db70410ee9eacd807b79c7a02bac70efb26606
Marcos Couto <marcos_otuoc@hotmail.com>
2020-06-25 09:32:07 -0300

    Initial commit

---
+++ src/test/kotlin/com/github/marcoscouto/cadastrosempresa/CadastrosEmpresaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.marcoscouto.cadastrosempresa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CadastrosEmpresaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
